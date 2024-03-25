4a352f26788d232ca663d0bf34c1cd8c1f483b7c
Guga Gongadze <gugongadze@gmail.com>
2024-03-25 11:46:04 +0400

    fix: display only female runners

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,23 +6,22 @@ import silver from '../images/silver.png';
 import bronze from '../images/bronze.png';
 
 const results = [
-  { img: gold, name: 'Guga Gongadze', distance: 44, time: '4:43:23' },
-  { img: silver, name: 'Gigi Arabuli', distance: 44, time: '5:07:36' },
-  { img: bronze, name: 'Kimi Papidze', distance: 44, time: '5:20:07' },
-  { img: gold, name: 'Ketevan Mumladze', distance: 44, time: '6:06:15' },
-  { img: silver, name: 'Sopho Jgharkava', distance: 44, time: '7:17:55' },
-  { img: bronze, name: 'Tamta Matitashvili', distance: 44, time: '7:18:13' },
+  { img: gold, name: 'Guga Gongadze', distance: 44, time: '4:43:23', gender: 'male' },
+  { img: silver, name: 'Gigi Arabuli', distance: 44, time: '5:07:36', gender: 'male' },
+  { img: bronze, name: 'Kimi Papidze', distance: 44, time: '5:20:07', gender: 'male' },
+  { img: gold, name: 'Ketevan Mumladze', distance: 44, time: '6:06:15', gender: 'female' },
+  { img: silver, name: 'Sopho Jgharkava', distance: 44, time: '7:17:55', gender: 'female' },
+  { img: bronze, name: 'Tamta Matitashvili', distance: 44, time: '7:18:13', gender: 'female' },
 ];
 
 export function Layout() {
+  const femaleRunners = results.filter((runner) => runner.gender === 'female');
+
   return (
     <div className="grid-container">
-      <Profile person={results[0]} />
-      <Profile person={results[1]} />
-      <Profile person={results[2]} />
-      <Profile person={results[3]} />
-      <Profile person={results[4]} />
-      <Profile person={results[5]} />
+      {femaleRunners.map((result) => (
+        <Profile key={result.name} person={result} />
+      ))}
     </div>
   );
 }
