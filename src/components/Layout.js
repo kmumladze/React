b585127ae8507d6922287e99ed915ce9bba3d00b
kmumladze <ketevanmumladze17@gmail.com>
2024-03-27 12:06:58 +0400

    refactor: use children props

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import './Layout.css';
-import { Profile } from './Profile';
-import gold from '../images/gold.png';
-import silver from '../images/silver.png';
-import bronze from '../images/bronze.png';
+import React from "react";
+import "./Layout.css";
+import { Profile } from "./Profile";
+import gold from "../images/gold.png";
+import silver from "../images/silver.png";
+import bronze from "../images/bronze.png";
 
 const results = [
-  { img: gold, name: 'Guga Gongadze', distance: 44, time: '4:43:23', gender: 'male' },
-  { img: silver, name: 'Gigi Arabuli', distance: 44, time: '5:07:36', gender: 'male' },
-  { img: bronze, name: 'Kimi Papidze', distance: 44, time: '5:20:07', gender: 'male' },
-  { img: gold, name: 'Ketevan Mumladze', distance: 44, time: '6:06:15', gender: 'female' },
-  { img: silver, name: 'Sopho Jgharkava', distance: 44, time: '7:17:55', gender: 'female' },
-  { img: bronze, name: 'Tamta Matitashvili', distance: 44, time: '7:18:13', gender: 'female' },
+  {
+    img: gold,
+    name: "Guga Gongadze",
+    distance: 44,
+    time: "4:43:23",
+    gender: "male",
+  },
+  {
+    img: silver,
+    name: "Gigi Arabuli",
+    distance: 44,
+    time: "5:07:36",
+    gender: "male",
+  },
+  {
+    img: bronze,
+    name: "Kimi Papidze",
+    distance: 44,
+    time: "5:20:07",
+    gender: "male",
+  },
+  {
+    img: gold,
+    name: "Ketevan Mumladze",
+    distance: 44,
+    time: "6:06:15",
+    gender: "female",
+  },
+  {
+    img: silver,
+    name: "Sopho Jgharkava",
+    distance: 44,
+    time: "7:17:55",
+    gender: "female",
+  },
+  {
+    img: bronze,
+    name: "Tamta Matitashvili",
+    distance: 44,
+    time: "7:18:13",
+    gender: "female",
+  },
 ];
 
 export function Layout() {
-  const femaleRunners = results.filter((runner) => runner.gender === 'female');
+  const femaleRunners = results.filter((runner) => runner.gender === "female");
 
   return (
     <div className="grid-container">
-      {femaleRunners.map((result) => (
-        <Profile key={result.name} person={result} />
+      {femaleRunners.map((runner) => (
+        <Profile key={runner.name}>
+          <img src={runner.img} alt="medal" className="medal" />
+          <h1 className="runner">{runner.name}</h1>
+          <h3 className="distance">Distance: {runner.distance}</h3>
+          <p className="time">Finish Time: {runner.time}</p>
+        </Profile>
       ))}
     </div>
   );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,5 @@
-import './Profile.css';
+import "./Profile.css";
 
-export function Profile({ person: { img, name, distance, time } }) {
-  return (
-    <div className="card">
-      <img src={img} alt="medal" className="medal" />
-      <h1 className="runner">{name}</h1>
-      <h3 className="distance">Distance: {distance}</h3>
-      <p className="time">Finish Time: {time}</p>
-    </div>
-  );
+export function Profile({ children }) {
+  return <div className="card">{children}</div>;
 }
