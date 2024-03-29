3b6330a9aea83d77b2078450149c3e6587c821a5
Guga Gongadze <gugongadze@gmail.com>
2024-03-29 11:08:45 +0400

    feat: filter runners

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,71 +1,101 @@
-import React from "react";
-import "./Layout.css";
-import { Profile } from "./Profile";
-import gold from "../images/gold.png";
-import silver from "../images/silver.png";
-import bronze from "../images/bronze.png";
+import React, { useState } from 'react';
+import './Layout.css';
+import { Profile } from './Profile';
+import gold from '../images/gold.png';
+import silver from '../images/silver.png';
+import bronze from '../images/bronze.png';
 
 const results = [
   {
     img: gold,
-    name: "Guga Gongadze",
+    name: 'Guga Gongadze',
     distance: 44,
-    time: "4:43:23",
-    gender: "male",
+    time: '4:43:23',
+    gender: 'male',
   },
   {
     img: silver,
-    name: "Gigi Arabuli",
+    name: 'Gigi Arabuli',
     distance: 44,
-    time: "5:07:36",
-    gender: "male",
+    time: '5:07:36',
+    gender: 'male',
   },
   {
     img: bronze,
-    name: "Kimi Papidze",
+    name: 'Kimi Papidze',
     distance: 44,
-    time: "5:20:07",
-    gender: "male",
+    time: '5:20:07',
+    gender: 'male',
   },
   {
     img: gold,
-    name: "Ketevan Mumladze",
+    name: 'Ketevan Mumladze',
     distance: 44,
-    time: "6:06:15",
-    gender: "female",
+    time: '6:06:15',
+    gender: 'female',
   },
   {
     img: silver,
-    name: "Sopho Jgharkava",
+    name: 'Sopho Jgharkava',
     distance: 44,
-    time: "7:17:55",
-    gender: "female",
+    time: '7:17:55',
+    gender: 'female',
   },
   {
     img: bronze,
-    name: "Tamta Matitashvili",
+    name: 'Tamta Matitashvili',
     distance: 44,
-    time: "7:18:13",
-    gender: "female",
+    time: '7:18:13',
+    gender: 'female',
   },
 ];
 
+// const femaleRunners = results.filter((runner) => runner.gender === 'female');
+
 export function Layout() {
-  const femaleRunners = results.filter((runner) => runner.gender === "female");
+  const [searchValue, setSearchValue] = useState('');
+  const [name, setName] = useState('');
+  const [runners, setRunners] = useState(results);
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value;
+
+    setSearchValue(newSearch);
+
+    const filteredRunners = results.filter((runner) => {
+      return runner.name.toLowerCase().includes(newSearch.toLowerCase());
+    });
+
+    setRunners(filteredRunners);
+  };
+
+  // const handleButtonClick = () => {
+  //   const filteredRunners = results.filter((runner) => {
+  //     return runner.name.toLowerCase().includes(searchValue.toLowerCase());
+  //   });
+
+  //   setRunners(filteredRunners);
+  // };
+
+  const handleNameClick = (event) => {
+    setName(event.target.textContent);
+  };
 
   return (
     <div className="content">
-      
+      <h1>{name}</h1>
       <div className="search-container">
-        <input type="text" placeholder="search"></input>
-        <button>search</button>
+        <input type="text" placeholder="search" value={searchValue} onChange={handleChange} />
+        {/* <button onClick={handleButtonClick}>search</button> */}
       </div>
 
       <div className="grid-container">
-        {femaleRunners.map((runner) => (
+        {runners.map((runner) => (
           <Profile key={runner.name}>
             <img src={runner.img} alt="medal" className="medal" />
-            <h1 className="runner">{runner.name}</h1>
+            <h1 className="runner" onClick={handleNameClick}>
+              {runner.name}
+            </h1>
             <h3 className="distance">Distance: {runner.distance}</h3>
             <p className="time">Finish Time: {runner.time}</p>
           </Profile>
